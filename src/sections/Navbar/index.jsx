ca525c567a196ca14512a535e4175d1f04ee5450
Tanveershazada <tanveershahsaae@gmail.com>
2023-08-16 00:52:55 +0500

    changes

--- src/sections/Navbar/index.jsx
+++ src/sections/Navbar/index.jsx
@@ -13,11 +13,25 @@ import "./style.css";
 import Button from "../../components/Button";
 import useScrollClientY from "../../hooks/useScrollClientY";
 import { CloseIcon, CloudIcon } from "../../components/Icons";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+
+const navLinks = [
+  { id: 1, name: "Home", link: "/" },
+  { id: 2, name: "About", link: "/about" },
+  { id: 3, name: "Pricing", link: "/pricing" },
+  { id: 3, name: "Contact", link: "/contact" },
+];
 
 function Header(props) {
+  const {pathname} = useLocation()
+  console.log(pathname)
   const scrollClientY = useScrollClientY();
   const [collapsed, setCollapsed] = useState(true);
+  const [active, setActive] = useState(pathname)
+
+  const handleActiveLink = (link) => {
+    setActive(link)
+  }
 
   const toggleNavbar = () => setCollapsed(!collapsed);
 
@@ -31,79 +45,55 @@ function Header(props) {
         <div className="container">
           <div className="d-block align-items-center d-md-flex">
             <div className="d-flex justify-content-between">
-
-            <NavbarBrand href="/" className="me-auto navbar__wrapper">
-              <img src={logo} alt="" className="img-fluid Navbar__logo" />
-              <span
-                className={`navbar__span ${
-                  scrollClientY > 200 ? "text-black" : "text-white"
-                } d-none d-md-inline-block`}
+              <NavbarBrand href="/" className="me-auto navbar__wrapper">
+                <img src={logo} alt="" className="img-fluid Navbar__logo" />
+                <span
+                  className={`navbar__span ${
+                    scrollClientY > 200 ? "text-black" : "text-white"
+                  } d-none d-md-inline-block`}
                 >
-                Social ignite Studio
-              </span>
-            </NavbarBrand>
-            <NavbarToggler
-              onClick={toggleNavbar}
-              className="me-2 toggleNavbar"
+                  Social ignite Studio
+                </span>
+              </NavbarBrand>
+              <NavbarToggler
+                onClick={toggleNavbar}
+                className="me-2 toggleNavbar"
               />
-              </div>
+            </div>
             <Collapse isOpen={!collapsed} navbar>
-              <Nav navbar className="ms-auto me-0 navbar__nav nav__list__main" >
-                <div className="d-flex d-md-none close__icon" onClick={() => setCollapsed(!collapsed)}><CloseIcon/></div>
-                <NavItem>
-                  {/* <NavLink
-                    className={`nav__items ${
-                      scrollClientY > 200 && "text-black"
-                    } active__cls`}
-                    href="/"
-                  >
-                    Home
-                  </NavLink> */}
-                  <Link to="/">Home</Link>
-                </NavItem>
-                <NavItem>
-                  {/* <NavLink
-                    className={`nav__items ${
-                      scrollClientY > 200 && "text-black"
-                    }`}
-                    href="/about"
-                  >
-                    About
-                  </NavLink> */}
-                  
-                  <Link to="/about">about</Link>
-                </NavItem>
-                <NavItem>
-                  <NavLink
-                    className={`nav__items ${
-                      scrollClientY > 200 && "text-black"
-                    }`}
-                    href="/Pricing"
-                  >
-                    Pricing
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink
-                    className={`nav__items ${
-                      scrollClientY > 200 && "text-black"
-                    }`}
-                    href="/contact"
-                  >
-                    Contact
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                <Button
-                  className={`navbar__btn ${
-                    scrollClientY > 200 ? "btn__primary" : "btn__secoundry"
-                  }`}
+              <Nav
+                navbar
+                className="ms-auto me-0 navbar__nav nav__list__main d-flex align-items-center"
+              >
+                <div
+                  className="d-flex d-md-none close__icon"
+                  onClick={() => setCollapsed(!collapsed)}
                 >
-                  GET STARTED
-                </Button>
+                  <CloseIcon />
+                </div>
+                {navLinks.map((item) => (
+                  <NavItem key={item.name} onClick={() => handleActiveLink(item.link)}>
+                    <Link
+                      className={`nav__items ${
+                        scrollClientY > 200 && "text-black"
+                      } ${active == item.link && 'nav__active__link'}`}
+                      to={item.link}
+                    >
+                      {item.name}
+                    </Link>
+                  </NavItem>
+                ))}
+
+                <NavItem>
+                  <Button
+                    className={`navbar__btn ${
+                      scrollClientY > 200 ? "btn__primary" : "btn__secoundry"
+                    }`}
+                  >
+                    GET STARTED
+                  </Button>
                 </NavItem>
               </Nav>
-                
             </Collapse>
           </div>
         </div>
